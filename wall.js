4d14b10c96a72b0238dfc44dd0dead82ea51bd36
Kurowp <jackc110@nycstudents.net>
2026-02-25 16:57:48 +0000

    update

--- wall.js
+++ wall.js
@@ -1,19 +1,19 @@
 
-class Wall{
-    constructor(x,y,z){
-        this.x = x;
-        this.y = y;
-        this.z = z;
+// class Wall{
+//     constructor(x,y,z){
+//         this.x = x;
+//         this.y = y;
+//         this.z = z;
     
-        this.obj = document.createElement("a-box");
-        this.obj.setAttribute("scale",{x:3,y:100,z:4});
-        this.obj.setAttribute("position",{x:x,y:y,z:z});
-        scene.append(this.obj);
+//         this.obj = document.createElement("a-box");
+//         this.obj.setAttribute("scale",{x:3,y:100,z:4});
+//         this.obj.setAttribute("position",{x:x,y:y,z:z});
+//         scene.append(this.obj);
 
 
 
 
 
 
-    }
-}
+//     }
+// }
--- wall2.js
+++ wall2.js
@@ -1,18 +1,18 @@
-class Wall2{
-    constructor(x,y,z){
-        this.x = x;
-        this.y = y;
-        this.z = z;
+// class Wall2{
+//     constructor(x,y,z){
+//         this.x = x;
+//         this.y = y;
+//         this.z = z;
     
-        this.obj = document.createElement("a-box");
-        this.obj.setAttribute("scale",{x:5,y:100,z:9});
-        this.obj.setAttribute("position",{x:x,y:y,z:z});
-        scene.append(this.obj);
+//         this.obj = document.createElement("a-box");
+//         this.obj.setAttribute("scale",{x:5,y:100,z:9});
+//         this.obj.setAttribute("position",{x:x,y:y,z:z});
+//         scene.append(this.obj);
 
 
 
 
 
 
-    }
-}
+//     }
+// }
